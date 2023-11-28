f7fe6df345d12da364d0a0e10b0f763cf8982bb4
Daniel Lenski <dlenski@gmail.com>
2023-11-27 17:13:43 -0800

    Add corosup CLI

---
+++ corostc/corosup.py
@@ -0,0 +1,30 @@
+import argparse
+from pprint import pprint
+from sys import stderr
+from getpass import getpass
+
+from . import CorosFileType, CorosSportType, CorosTCClient
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('-u', '--username')
+    p.add_argument('-p', '--password')
+    p.add_argument('fitfile', nargs='+')
+    args = p.parse_args()
+
+    if not args.username:
+        print('COROS Training Center Username: ', end='')
+        args.username = read()
+    if not args.password:
+        args.password = getpass('COROS Training Center Password: ')
+
+    client = CorosTCClient(args.username, args.password)
+    client.connect()
+    for fitfile in args.fitfile:
+        j = client.upload_activity(open(fitfile, 'rb'), compress=False)
+        pprint(j)
+    else:
+        print(f'Uploaded {len(args.fitfile)} files')
+
+if __name__=='__main__':
+    main()
